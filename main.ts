2b3590cbf9db5216909e31b337a5770b082beb94
aydin2009 <66886643+aydin2009@users.noreply.github.com>
2020-06-18 13:28:11 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+
+// This function prints out your name and favorite color
+
+function nameAndColor(name:string, Color:string) {
+   console.log ("Hello " + name + ". Your favorite color is " + Color  )  
+}
+
+nameAndColor( "Aydin" , "Blue")
+
+// This function prints out you name and favorite color
+
+function nameAndNumber(name:string, favNum:number) {
+    console.log ("Hello " + name + ".Your favorite number is " + favNum)
+
+    
+}
+
+nameAndNumber( "Aydin" , 2)
