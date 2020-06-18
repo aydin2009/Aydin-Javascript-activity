456546c22eda57c521c761c783f1d5fb6f1d77c2
aydin2009 <66886643+aydin2009@users.noreply.github.com>
2020-06-18 14:39:16 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -16,3 +16,72 @@ function nameAndNumber(name:string, favNum:number) {
 }
 
 nameAndNumber( "Aydin" , 2)
+
+//make a new function that prints the users name and and grade
+
+function nameAndGrade(name:string, grade:number) {
+    console.log ("Hello " + name + ". Your grade is " + grade)
+
+    
+}
+nameAndGrade("Aydin", 5)
+
+//make a new function that prints the users name and school
+
+function nameAndSchool(name:string, school:string) {
+    console.log ("Hello " + name + ". Your school is " + school)
+    
+}
+nameAndSchool("Aydin", "Garden Gate Elementry School")
+
+//Make a new function that prints out the users name , grade and school
+
+function nameGradeSchool(name:string, Grade:number , school:string ) {
+    console.log ("Hello " + name + ". Your grade is "+ Grade + " at the school " + school  )
+
+
+    
+}
+
+nameGradeSchool("Aydin ", 5 , "Garden Gate Elementry School" )
+
+// This function multiplys a number by 2
+
+function multiplyByTwo(startingNum:number){
+    let result = startingNum *2
+    console.log (result)
+ }  
+
+ multiplyByTwo(10)
+
+ //This function multiplys a number by 5
+
+ function multiplyByFive(startingNum:number) {
+     let result = startingNum *5
+     console.log (result)
+     
+ }
+    multiplyByFive(15)
+
+
+//This function divides a number by 2 
+
+function divideByTwo(startingNum:number) {
+    let result = startingNum / 2
+    console.log (result)
+    
+}  
+divideByTwo(220)
+    
+//This function that divides a number by 5 and subtracts the number by 20
+
+function divideByFiveSubtractByTwenty(startingNum:number) {
+    let result = startingNum / 5 - 20
+    console.log (result)    
+}
+divideByFiveSubtractByTwenty(100)
+
+
+
+
+
